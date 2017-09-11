93a90362805a88cc41baf44834b86406752d4a2f
Richard Beddington <richard.beddington@gmail.com>
2017-09-11 09:52:11 +0100

    Attempt XDR on IE9

--- src/utils/WebAPIUtils.js
+++ src/utils/WebAPIUtils.js
@@ -1,15 +1,35 @@
 import { queryStringFromObject } from './URIUtils';
 
+// TODO: handle failures!
+
 // TODO: provide this through other means!
 const API_URL = 'https://content.guardianapis.com';
 const API_KEY = '9wur7sdh84azzazdt3ye54k4';
+
+const compatFetch = (uri) => {
+  // TODO: check actually XDomain!
+
+  if (!window.fetch && window.XDomainRequest) {
+    return new Promise((resolve) => {
+      const xdr = new XDomainRequest();
+
+      xdr.open(uri);
+
+      xdr.onload = () => {
+        resolve(xdr.responseText());
+      };
+    });
+  }
+
+  return fetch(uri).then(response => response.json());
+};
 
 // TODO: add path '/' if not present
 // TODO: catch errors
 const contentAPIQuery = (path, queryObj) => {
   const queryString = queryStringFromObject(queryObj);
   const uri = `${API_URL}${path}${queryString}`;
-  return fetch(uri).then(response => response.json());
+  return compatFetch(uri);
 };
 
 const search = (options = {}) =>
